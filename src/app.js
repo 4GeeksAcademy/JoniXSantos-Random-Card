6bc41cbb1048f47e78ce3e04e67735f2bf208636
Joni Santos <jonisantos1996@hotmail.com>
2024-08-11 17:40:08 +0000

    feat: random card generator

--- src/app.js
+++ src/app.js
@@ -4,8 +4,57 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { mapToStyles } from "@popperjs/core/lib/modifiers/computeStyles";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generateCard();
+  setInterval(generateCard, 10000);
+  let button = document.querySelector("#newCard");
+  button.addEventListener("click", generateCard);
 };
+
+let suits = ["♦", "♥", "♠", "♣"];
+let numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
+function randomSuit() {
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+  return suit;
+}
+
+function randomNumber() {
+  let number = numbers[Math.floor(Math.random() * numbers.length)];
+  return number;
+}
+
+function changeColor(suit) {
+  if (suit == "♦" || suit == "♥") {
+    return "red";
+  } else {
+    return "black";
+  }
+}
+
+function generateCard() {
+  let card = document.querySelector(".card");
+  let sameSuit = randomSuit();
+  card.innerHTML = `<p class="top-suit text-start">${sameSuit}</p>
+    <p class="number text-center">${randomNumber()}</p>
+    <p class="bottom-suit text-start">${sameSuit}</p>`;
+  card.style.color = changeColor(sameSuit);
+  let container = document.querySelector(".container");
+  container.appendChild(card);
+}
